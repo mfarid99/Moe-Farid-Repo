2ca9e48ffdcf16a44028f6cc1a82ada6880a2931
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-09-13 16:44:21 -0400

    express progress cheese

--- Express_Practice/index.js
+++ Express_Practice/index.js
@@ -1,1 +1,16 @@
-console.log('hello')
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res)=> {
+    res.send("hello moe")
+})
+
+app.get('/cheese/:variable', (req, res)=> {
+    res.send(`${req.params.variable}  ${req.query.cheese}`)
+})
+
+
+app.listen(3003, () => {
+    console.log('wuzup')
+})
