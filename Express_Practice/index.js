f6af317b2c51a1f808d755b97a841c85152f42d2
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-09-14 20:14:06 -0400

    further more express

--- Express_Practice/index.js
+++ Express_Practice/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'));
 
 app.get('/', (req, res)=> {
-    res.send("hello moe")
-})
+    res.render('index.ejs', {hello: 'Hello Halambuha'});
+});
 
 app.get('/cheese/:variable', (req, res)=> {
     res.send(`${req.params.variable}  ${req.query.cheese}`)
